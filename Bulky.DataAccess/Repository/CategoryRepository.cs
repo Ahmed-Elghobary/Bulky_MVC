da743c42de4c879cb55c5af03c0f022b3c5c1989
Ahmed_Osama <ahmed.elghobary01@gmail.com>
2024-04-13 00:10:13 +0200

    Azure Deploy

--- Bulky.DataAccess/Repository/CategoryRepository.cs
+++ Bulky.DataAccess/Repository/CategoryRepository.cs
@@ -13,7 +13,7 @@ namespace BulkyBook.DataAccess.Repository
     public class CategoryRepository :Repository<Category> ,ICategoryRepository
     {
         private  ApplicationDbContext _db;
-        public CategoryRepository(ApplicationDbContext db):base(db) 
+        public CategoryRepository(ApplicationDbContext db) : base(db)
         {
             _db= db;
         }
